77c1043498188d16fc4693675ffbea5d7a168412
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-09-15 17:41:38 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,15 @@
-bin/pulsar-admin sources delete \
-    --tenant public \
-    --namespace default \
-    --name solr-sink-iot
+bin/pulsar-admin sink stop --name solr-sink-iot --namespace default --tenant public
 
-bin/pulsar-admin sinks create \
-    --tenant public \
-    --namespace default \
-    --name solr-sink-iot \
-    --sink-type solr \
-    --sink-config-file conf/solr-sink.yml \
-    --inputs nvidia-sensor
+bin/pulsar-admin sinks delete --tenant public --namespace default --name solr-sink-iot
+
+bin/pulsar-admin sinks create --tenant public --namespace default --name solr-sink-iot --sink-type solr --sink-config-file conf/solr-sink.yml --inputs nvidia-sensor
     
-bin/pulsar-admin sinks get \
-    --tenant public \
-    --namespace default \
-    --name solr-sink-iot
+bin/pulsar-admin sinks get --tenant public --namespace default --name solr-sink-iot
 
-bin/pulsar-admin sinks list \
---tenant public \
---namespace default
+bin/pulsar-admin sinks list --tenant public --namespace default
 
 curl 'http://localhost:8080/admin/v3/source/public/default'
 
-bin/pulsar-admin sinks status \
---tenant public \
---namespace default \
---name solr-sink-iot
+bin/pulsar-admin sinks status --tenant public --namespace default --name solr-sink-iot
 
 echo "Topic: nvidia-sensor"
